a9456325c3b2e9f0cbac25adb0dac54c07bb1bae
i_junghoon <ljh37694@naver.com>
2024-09-08 16:22:15 +0900

    게시글 보여주는 ui 구현

--- chatmunity/app/main/page.tsx
+++ chatmunity/app/main/page.tsx
@@ -1,8 +1,72 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './page.module.css';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+
+interface Post {
+  title: string,
+  id?: string,
+}
+
 export default function Main() {
+  const postList: Post[] = [
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+    { title: "내용" },
+  ];
+
   return (
-    <div>
-      <h3>제목</h3>
-      <p>동해물과 백두산이 마르고 닳도록 하느님이 보우하사 우리나라 만세</p>
+    <div className={styles.container}>
+      <nav className={styles.topNav}>
+        <div className={styles.box}></div>
+        <h3 className={styles.title}>어쩌고 저쩌구</h3>
+        <div className={styles.box}>
+          <label className={styles.searchBtn}>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+          </label>
+        </div>
+      </nav>
+      <section className={styles.postContainer}>
+        {postList.map((item, idx) => {
+          return (
+            <div className={`${styles.post} ${idx % 2 === 0? styles.hotPost : ''}`} key={idx}>
+              <p>{item.title}</p>
+            </div>
+          );
+        })}
+      </section>
     </div>
   );
 }
