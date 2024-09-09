b9220a0a4f8df78f6713013f7a596f9d3478de0a
i_junghoon <ljh37694@naver.com>
2024-09-09 13:29:27 +0900

    기본 버튼 컴포넌트

---
+++ chatmunity/components/Button.tsx
@@ -0,0 +1,15 @@
+import styles from './Button.module.css';
+
+interface Props {
+  text: string,
+  className: string,
+  [key: string]: any,
+}
+
+export default function Button(props: Props) {
+  const { text, className, ...rest } = props;
+
+  return (
+    <button className={`${styles.button} ${className}`} {...rest}>{text}</button>
+  );
+}
