3f322d62d4e7bc8687cea48794f3ffeca01372fa
i_junghoon <ljh37694@naver.com>
2024-09-10 13:46:30 +0900

    mongodb 설치

---
+++ chatmunity/app/utils/datadbase.ts
@@ -0,0 +1,17 @@
+import { MongoClient } from 'mongodb'
+
+const url = `mongodb+srv://ljh37694:${process.env.DB_PASSWORD}@forum.6p5dx3j.mongodb.net/?retryWrites=true&w=majority&appName=Forum`;
+const options: any = { useNewUrlParser: true };
+let connectDB: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === 'development') {
+  // 개발 중 재실행을 막음
+  if (!global._mongo) {
+    global._mongo = new MongoClient(url, options).connect();
+  }
+  connectDB = global._mongo;
+} else {
+  connectDB = new MongoClient(url, options).connect();
+}
+
+export { connectDB }
