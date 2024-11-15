4e8dc775a82e8af00452edc3418abfc2b1dc56fb
i_junghoon <ljh37694@naver.com>
2024-11-15 13:12:29 +0900

    post room 디자인 수정 및 제목 대신 내용이 보이던 버그 수정

--- chatmunity/app/room/[id]/page.tsx
+++ chatmunity/app/room/[id]/page.tsx
@@ -1,7 +1,7 @@
 import PostRoom from "@/components/ui/PostRoom";
 import styles from "./page.module.css";
 import { connectDB } from "@/app/utils/datadbase";
-import { Post, PostRoomType } from "@/types";
+import { Chat, Post, PostRoomType } from "@/types";
 import ChattingRoom from "@/components/common/ChattingRoom";
 import ChattingList from "@/components/common/ChattingList";
 import PostInput from "@/components/ui/PostInput";
@@ -17,9 +17,19 @@ export default async function RoomDetail(props: Props) {
   const client = await connectDB;
   const db = client.db('Chatmunity');
 
-  const postList = await db.collection<Post>('post').find({
+  const posts = await db.collection<Post>('post').find({
     room_id: props.params.id,
   }).toArray();
+
+  const postList: Chat[] = posts.map((item) => { 
+    return {
+      content: item.title,
+      date: item.date,
+      name: item.name,
+      writer: item.writer,
+      _id: item._id.toString(),
+    }
+  });
   
   const roomData = await db.collection<PostRoomType>('room').findOne({ id: props.params.id });
 
@@ -28,13 +38,8 @@ export default async function RoomDetail(props: Props) {
       <ChattingList inputComp={<PostInput roomId={props.params.id} />}>
         {
           postList.map((item, idx) => {
-            const data: Post = {
-              ...item,
-              _id: item._id.toString(),
-            }
-
             return (
-              <Chatting chatData={data} isOtherChat={idx % 2 === 0} url={`/post/${item._id}`} key={item._id.toString()} />
+              <Chatting chatData={item} isOtherChat={idx % 2 === 0} url={`/post/${item._id}`} key={item._id as string} />
             );
           })
         }
