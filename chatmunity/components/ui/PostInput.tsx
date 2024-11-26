97d34ddfe686d917d65c9ec78cf25714edb99b45
i_junghoon <ljh37694@naver.com>
2024-11-26 13:14:05 +0900

    PostInput Input컴포넌트를 사용해서 구현

--- chatmunity/components/ui/PostInput.tsx
+++ chatmunity/components/ui/PostInput.tsx
@@ -4,6 +4,7 @@ import styles from '@/styles/ui/PostInput.module.css';
 import Button from '../common/Button';
 import { useRouter } from 'next/navigation';
 import { useRef, useState } from 'react';
+import Input from '../common/Input';
 
 interface Props {
   roomId: string,
@@ -17,24 +18,21 @@ export default function PostInput(props: Props) {
 
   const [text, setText] = useState<string>('');
 
-  console.log(props);
+  const onSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!inputRef.current?.value) {
+      alert('빈 칸입니다');
+    } else {
+      console.log(inputRef.current.value);
+
+      const title: string = inputRef.current.value;
+
+      router.push('/write/' + props.roomId + "?title=" + encodeURIComponent(title));
+    }
+  }
 
   return (
-    <form className={`${styles.form} ${props.className}`} onSubmit={(e: React.FormEvent) => {
-      e.preventDefault();
-
-      if (!inputRef.current?.value) {
-        alert('빈 칸입니다');
-      } else {
-        console.log(inputRef.current.value);
-
-        const title: string = inputRef.current.value;
-
-        router.push('/write/' + props.roomId + "?title=" + encodeURIComponent(title));
-      }
-    }}>
-      <input className={styles.input} value={text} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setText(e.target.value)} ref={inputRef} />
-      <Button text='글쓰기' className={styles.button} />
-    </form>
+    <Input buttonText='글쓰기' onSubmit={onSubmit} textState={text} setText={setText} />
   );
 }
